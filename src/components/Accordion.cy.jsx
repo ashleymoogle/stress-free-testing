bccebf9361a345a33c2f28e84aa970020e4ad4dd
Jessica Sachs <jess@jessicasachs.io>
2022-11-01 15:59:43 -0400

    adding solution

--- src/components/Accordion.cy.jsx
+++ src/components/Accordion.cy.jsx
@@ -1,24 +1,41 @@
 import Accordion from './Accordion.vue'
+import { faker } from '@faker-js/faker'
 
 const headerSelector = '[data-testid=header]'
 const contentSelector = '[data-testid=content]'
 
+const headerContent = faker.lorem.lines(1)
+const content = faker.lorem.paragraphs(3)
+
 describe('<Accordion />', () => {
   it('renders', () => {
     const slots = {
-      default: () => <p data-testid="content">Content Within the slot</p>,
-      target: () => <h1 data-testid="header">Header</h1>,
+      default: () => <p data-testid="content">{content}</p>,
+      target: () => <h1 data-testid="header">{headerContent}</h1>,
     }
 
     cy.mount(() => (
       <div class="p-12">
         <h1 class="text-lg">Accordion Examples</h1>
         <h1 class="pb-8">Click on the header to toggle the card</h1>
+        {/* Arrange */}
         <Accordion vSlots={slots}></Accordion>
       </div>
     ))
+    .get(headerSelector).should('be.visible').and('contain.text', headerContent)
+    .get(contentSelector).should('not.exist')
+
+    // Acting
+    .get(headerSelector).click()
+    .get(contentSelector).should('be.visible')
 
     // Use the header selectors and content selectors to interact
     // with the accordion and test that it correctly expands and collapses.
+  })
+
+  describe('props', () => {
+    it('does not render content when initiallyOpen is false', () => {})
+    it('renders content when initiallyOpen is true', () => {})
+    it('is collapsed by default', () => {})
   })
 })
